2c961ed7fa79d424b9c522e6d473fe9eb21c0382
Robbert Broersma <mail@robbertbroersma.nl>
2021-07-19 23:18:35 +0200

    feat: NL Design System showcase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { UtrechtWebComponentsModule } from '@utrecht/web-component-library-angular';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UtrechtWebComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { defineCustomElements } from '@utrecht/web-component-library-stencil';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
+
+defineCustomElements();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
