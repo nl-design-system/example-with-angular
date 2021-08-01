3e5cfe6df1083a33ca3a56d4720211c9b40a2943
Robbert Broersma <mail@robbertbroersma.nl>
2021-08-02 00:42:57 +0200

    feat: Angular form bindings for textbox and checkbox form fields

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UtrechtWebComponentsModule } from '@utrecht/web-component-library-angular';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UtrechtWebComponentsModule],
+  imports: [BrowserModule, ReactiveFormsModule, UtrechtWebComponentsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
