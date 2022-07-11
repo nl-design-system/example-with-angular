399d4d49a3d6b513d275b559cf0e9b5073f2e862
Robbert Broersma <mail@robbertbroersma.nl>
2022-07-11 12:56:35 +0200

    refactor: switch from `app` to `example` prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { FormControl } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'example-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
