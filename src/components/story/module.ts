b79dd21bacec5397b03d22d5ec02bd7f2a72b346
Ali Amori Kadhim <ali@frameless.io>
2022-07-13 16:13:29 +0200

    fix: remove module.ts
    because it's already registered in src/components/index.ts

--- src/components/story/module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ExampleStoryComponent } from './component';
-
-@NgModule({
-  imports: [],
-  exports: [ExampleStoryComponent],
-  declarations: [ExampleStoryComponent],
-})
-export class ExampleStoryModule {}
