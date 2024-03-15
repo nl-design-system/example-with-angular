e09582b9c4a1943dc8ca38084e2e8377782c30d6
matijs <matijs@gmail.com>
2024-03-15 08:15:23 +0100

    build: pre-update improvements
    - Add "save-exact=true" to `.npmrc`
    - Move the npm-check-updates configuration from command-line arguments
      in `package.json#scripts` into the respective `.ncurc.*.cjs` files
    - Makes sure `*.cjs` files are linted

--- .ncurc.major.cjs
+++ .ncurc.major.cjs
@@ -1,5 +1,7 @@
-const minorConfig = require("./.ncurc.minor.cjs");
+const minorConfig = require('./.ncurc.minor.cjs');
 
 module.exports = {
+  ...minorConfig,
   reject: [...minorConfig.reject],
+  target: 'latest',
 };
